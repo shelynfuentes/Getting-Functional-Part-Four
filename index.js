bd583a22542443a902ee026ce0d9f7b7c2488da4
Jonathan Corley <>
2021-04-12 15:37:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//DO NOT CHANGE CODE HERE
+var customers = require('./customers.json');
+//DO NOT CHANGE CODE HERE
+
+
+/*
+
+Write a function that can count the number of
+genders in the dataset. The function will 
+accept a string and an array as arguements. 
+The output should be a number. 
+
+*/
+
+function genderCount(array, string){
+//CODE BELOW HERE
+
+
+
+//CODE ABOVE HERE
+}
